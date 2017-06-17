c3a008464a0261e2d7cd8041db2fe58fbf1c8f72
Jonathan Larson <jonathanl@eleveninc.com>
2017-06-17 11:01:46 -0700

    copy edits

--- footer.php
+++ footer.php
@@ -29,9 +29,9 @@
 			</div>
 			<div class="grid__item">
 				<ul class="site-footer-list site-footer-list--social">
-				<li><a class="site-footer-list__icon site-footer-list__icon--instagram" href="">Instagram</a></li>
-				<li><a class="site-footer-list__icon site-footer-list__icon--twitter" href="">Twitter</a></li>
-				<li><a class="site-footer-list__icon site-footer-list__icon--facebook" href="facebook">Facebook</a></li>
+				<li><a class="site-footer-list__icon site-footer-list__icon--instagram" href="https://www.instagram.com/deepfilmsrob/" target="_blank">Instagram</a></li>
+				<li><a class="site-footer-list__icon site-footer-list__icon--twitter" href="https://twitter.com/deepfilmsRob" target="_blank">Twitter</a></li>
+				<li><a class="site-footer-list__icon site-footer-list__icon--facebook" href="https://www.facebook.com/DeepFilms/" target="_blank">Facebook</a></li>
 			</ul>
 			</div>
 
